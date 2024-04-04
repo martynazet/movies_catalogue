a9dc07fa367871dabc2a533fabf0a033550c8230
Martyna Zawór <mart.zaw.95@gmail.com>
2024-04-04 23:27:45 +0200

    Generate movies

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from flask import Flask, render_template
+import tmdb_client
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def homepage():
-    movies = [{"title": "Film 1", "description": "Opis filmu 1", "image_url": "link_do_obrazka1.jpg"},
-    {"title": "Film 2", "description": "Opis filmu 2", "image_url": "link_do_obrazka2.jpg"},
-    {"title": "Film 3", "description": "Opis filmu 3", "image_url": "link_do_obrazka3.jpg"},
-]
+    movies = tmdb_client.get_movies()(how_many=8)
     return render_template('homepage.html', movies=movies)
+
+@app.context_processor
+def utility_processor():
+    def tmdb_image_url(path, size):
+        return tmdb_client.get_poster_url(path, size)
+    return {'tmdb_image_url': tmdb_image_url}
 
 
 if __name__ == '__main__':
